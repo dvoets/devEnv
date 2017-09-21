6cc8b83d11855d8c553bbd659b6a2de6900a6bb0
Daan Voets <daan.voets@sas.com>
2017-09-21 06:37:31 -0400

    add zsh customization

--- install.sh
+++ install.sh
@@ -2,3 +2,5 @@ cp ~/devEnv/dotfiles/.* .
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 vim +PluginInstall +qall
 cp -r  ~/.vim/bundle/molokai/colors ~/.vim
+
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
