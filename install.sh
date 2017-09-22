ca26d34e0b56db73be7628a54b9527747c95bc5a
Daan Voets <daan.voets@sas.com>
2017-09-22 11:31:24 -0400

    improved the install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,18 @@
-cp ~/devEnv/dotfiles/.* .
+rm ~/.*rc
+ln -s /home/install/devEnv/dotfiles/.alias ~/.alias
+ln -s /home/install/devEnv/dotfiles/.bashrc ~/.bashrc
+ln -s /home/install/devEnv/dotfiles/.tmux.conf ~/.tmux.conf
+ln -s /home/install/devEnv/dotfiles/.vimrc ~/.vimrc
+ln -s /home/install/devEnv/dotfiles/.zshrc ~/.zshrc
+
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 vim +PluginInstall +qall
-cp -r  ~/.vim/bundle/molokai/colors ~/.vim
 
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+mkdir ~/.vim/colors
+ln -s ~/.vim/bundle/molokai/colors/molokai.vim ~/.vim/colors/molokai.vim
+
+sudo sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+sudo chsh -s /bin/bash
+
+sudo yum install tmux -y
